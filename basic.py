711e5af8a6807f6300d8be6d63d5690a18daae74
haohanc2 <chenghaohan97@gmail.com>
2020-07-23 15:47:46 -0500

    no message

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+import copy
 
 from PIL import Image
 import numpy as np
@@ -16,38 +17,49 @@ def inbound(x,y,n,m):
     return x>=0 and x<n and y>=0 and y<m
 
 def blur_image(url,area):
-    a = np.array(Image.open(url))
+    a = np.array(Image.open(url),dtype=np.int)
     n=len(a)
     m=len(a[0])
-    b=a
+    b= copy.deepcopy(a)
     dp=a
     for i in range(1,n):
         dp[i][0]+=dp[i-1][0]
 
     for j in range(1,m):
         dp[0][j]+=dp[0][j-1]
-    print(a)
-    print('weqweqwe')
+
 
     for i in range(1,n):
         for j in range(1,m):
             dp[i][j]+=(dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1])
-    print(dp)
+    print(dp-a)
     for i in range(n):
         for j in range(m):
             print(i,j)
-            x1=max(i-area,0)
+            x1=max(i-area-1,0)
             x2=min(i+area,n-1)
-            y1=max(j-area,0)
+            y1=max(j-area-1,0)
             y2=min(j+area,m-1)
             count=(y2-y1+1)*(x2-x1+1)
             new=dp[x2][y2]-dp[x2][y1]-dp[x1][y2]+dp[x1][y1]
-            new=new/count
+            new=new//count
             b[i][j]=new
     print(b)
-    im = Image.fromarray(dp.astype('uint8'))
+    im = Image.fromarray(b.astype('uint8'))
     im.save('4.jpg')
 
+    for i in range(n):
+        for j in range(m):
+            x1 = max(i - area - 1, 0)
+            x2 = min(i + area, n - 1)
+            y1 = max(j - area - 1, 0)
+            y2 = min(j + area, m - 1)
+            count = (y2 - y1 + 1) * (x2 - x1 + 1)
+            new=(0,0,0)
+            for k in range(x1,x2):
+                for q in range(y1,y2):
+                    new+=a
 
 
-blur_image('1.jpg',3)
+
+blur_image('1.jpg',20)
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from PIL import Image
 import numpy as np
-y = np.array([[1,2,3],[2,3,4]],[[2,3,4],[3,4,5]])
+c = np.array([[[1,2,3],[2,3,4]],[[2,3,4],[3,4,5]]])
+y=c
 print(y)
+n=len(y)
+m=len(y[0])
+for i in range(1,n):
+    y[i][0]+=y[i-1][0]
 
-for i in range(1,len(y)):
-    for j in range(1,len(y[0])):
-        y[i][j]+=(y[i-1][j]+y[i][j-1]+y[i-1]
+for j in range(1,m):
+    y[0][j]+=y[0][j-1]
 
-print(y)
+for i in range(1,n):
+    for j in range(1,m):
+        y[i][j]+=(y[i-1][j]+y[i][j-1]-y[i-1][j-1])
+print(c)
+
 
